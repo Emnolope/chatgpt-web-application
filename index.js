157b6620d069cfc9de10b83b05819e15763fabe0
Emnolope <emmanuelnlopez+15@gmail.com>
2023-10-20 23:55:35 +0000

    index.js mod chatGPT4 advice?

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { createServer } = require('http');
 const express = require('express');
 const {Configuration, OpenAIApi} = require("openai");
 const app = express();
@@ -102,5 +103,5 @@ app.post('/get-prompt-result', async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const server = createServer(app);
+module.exports = server;
