870650627030cde563b50350c3bb453c60df8e1c
Emnolope <44859532+Emnolope@users.noreply.github.com>
2023-12-05 11:11:58 -0600

    added Vercel and Gitpod mode comments

--- index.js
+++ index.js
@@ -136,8 +136,10 @@ app.post('/get-prompt-result', async (req, res) => {
     }
 });
 
+//Vercel mode
 const server = createServer(app);
 module.exports = server;
 
+//Gitpod mode
 //const port = process.env.PORT || 3001;
 //app.listen(port, () => console.log(`Listening on port ${port}`));
